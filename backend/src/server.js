26e0bb2d22515a64c881964cf59f35f045ca3db9
P4nda <cuongvu513@gmail.com>
2025-12-01 21:38:55 +0700

    feat(auth,user): tạo tính năng thêm, sửa, đọc người dùng

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,20 +1,34 @@
-const express = require('express');
-const path = require('path');
+require("dotenv").config();
+const express = require("express");
+const morgan = require("morgan");
+const cors = require("cors");
+
+const apiRoutes = require("./routes");
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
-// phục vụ file tĩnh từ thư mục "src/public"
-app.use(express.static(path.join(__dirname, 'public')));
+// Middlewares
+app.use(cors());
+app.use(express.json());
+app.use(morgan("dev"));
 
-// route mặc định
-app.get('/', (req, res) => {
-  res.send(`
-    <h1>Xin chào từ server Node.js 👋</h1>
-    <p>Nếu bạn đọc được dòng này thì npm và server đều chạy OK.</p>
-  `);
+// API entry
+app.use("/api", apiRoutes);
+
+// 404 handler
+app.use((req, res) => {
+  res.status(404).json({ error: "Not found" });
+});
+
+// error handler
+app.use((err, req, res, next) => {
+  console.error("🔥 ERROR:", err);
+  res.status(err.status || 500).json({
+    error: err.message || "Internal error",
+  });
 });
 
 app.listen(PORT, () => {
-  console.log(`Server đang chạy tại http://localhost:${PORT}`);
+  console.log(`🚀 Server running at http://localhost:${PORT}`);
 });
