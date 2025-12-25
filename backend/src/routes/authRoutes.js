7c433a6a4b99c5ebf6d0aa9e53eeb29c6460ff3a
Bùi Đạt-1802 <buixuandat1802@gmail.com>
2025-12-25 15:55:03 +0700

    feat(auth): tìm kiếm lớp học

--- backend/src/routes/authRoutes.js
+++ backend/src/routes/authRoutes.js
@@ -12,4 +12,7 @@ router.post("/logout", authController.logout);
 router.post("/forgot-password", authController.forgotPassword); // quên mật khẩu
 router.post("/reset-password", authController.resetPassword);   // đặt lại mật khẩu
 
+
+router.get("/classes/search", authController.searchClassesByName);    // tìm kiếm lớp học theo tên
+
 module.exports = router;
